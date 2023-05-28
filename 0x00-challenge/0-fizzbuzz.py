f9f9222aac0cc932bb5052ff94bd4e0fa728defa
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-05-28 10:28:38 +0000

    done

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -18,7 +18,7 @@ def fizzbuzz(n):
     tmp_result = []
     for i in range(1, n + 1):
         rem = i / 3
-        if (i % 3) == 0 and (rem % 3) == 0:
+        if (i % 3) == 0 and (i != 15):
             tmp_result.append("Fizz")
         elif (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
